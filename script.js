c03cf7461098acf7dd5c9ba834e93f2967dd7b34
Martin C <martin.czabadaj@gmail.com>
2019-08-21 16:40:07 +0200

    calculs fonctionnels

--- script.js
+++ script.js
@@ -14,15 +14,13 @@ let stringResult = "";
 numbers.forEach((button) => {
     button.addEventListener("click", () => {
         stringOperation += button.textContent;
-        console.log(stringOperation);
         operation.textContent = stringOperation;
     })
 })
 
 operators.forEach((button) => {
     button.addEventListener("click", () => {
-        stringOperation += button.textContent;
-        console.log(stringOperation);
+        stringOperation += " " + button.textContent + " ";
         operation.textContent = stringOperation;
     })
 })
@@ -34,3 +32,68 @@ clear.addEventListener("click", () => {
     result.textContent = stringResult;
 
 })
+
+equal.addEventListener("click", () => {
+    let array = stringOperation.split(" ");
+    console.log(array);
+    while (array.indexOf("*") >= 0) {
+        let index = array.indexOf("*");
+        array[index - 1] = operate("*", array[index - 1], array[index + 1]);
+        array.splice(index, 2);
+    }
+    while (array.indexOf("/") >= 0) {
+        let index = array.indexOf("/");
+        array[index - 1] = operate("/", array[index - 1], array[index + 1]);
+        array.splice(index, 2);
+    }
+    while (array.indexOf("+") >= 0) {
+        let index = array.indexOf("+");
+        array[index - 1] = operate("+", array[index - 1], array[index + 1]);
+        array.splice(index, 2);
+    }
+    while (array.indexOf("-") >= 0) {
+        let index = array.indexOf("-");
+        array[index - 1] = operate("-", array[index - 1], array[index + 1]);
+        array.splice(index, 2);
+    }
+    console.log(array[0]);
+    stringResult = "= " + array[0].toString();
+    result.textContent = stringResult;
+})
+
+
+const add = function(a, b) {
+    let aNumber = +a;
+    let bNumber = +b;
+    return aNumber + bNumber;
+}
+
+const sub = function(a, b) {
+    let aNumber = +a;
+    let bNumber = +b;
+    return aNumber - bNumber;
+}
+
+const mult = function(a, b) {
+    let aNumber = +a;
+    let bNumber = +b;
+    return aNumber * bNumber;
+}
+
+const div = function(a, b) {
+    let aNumber = +a;
+    let bNumber = +b;
+    return aNumber / bNumber;
+}
+
+const operate = function(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return sub(a, b);
+    } else if (operator === "*") {
+        return mult(a, b);
+    } else if (operator === "/") {
+        return div(a, b);
+    } else { return "error"; }
+}
