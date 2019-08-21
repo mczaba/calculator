66811e67b4b4a42710f6be8bc69e9ad1dce7ec76
Martin C <martin.czabadaj@gmail.com>
2019-08-21 14:52:17 +0200

    affichage boutons ok

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let clear = document.querySelector("#clear");
+let equal = document.querySelector("#equal");
 let display = document.querySelector("#display");
+let operation = document.querySelector("#operation");
+let result = document.querySelector("#result");
+
+let stringOperation = "";
+let stringResult = "";
+
 
 
 numbers.forEach((button) => {
     button.addEventListener("click", () => {
-        console.log(button.textContent);
-        let string = document.createElement("p");
-        string.textContent = button.textContent;
-        display.appendChild(string);
+        stringOperation += button.textContent;
+        console.log(stringOperation);
+        operation.textContent = stringOperation;
     })
+})
+
+operators.forEach((button) => {
+    button.addEventListener("click", () => {
+        stringOperation += button.textContent;
+        console.log(stringOperation);
+        operation.textContent = stringOperation;
+    })
+})
+
+clear.addEventListener("click", () => {
+    stringOperation = "";
+    stringResult = "";
+    operation.textContent = stringOperation;
+    result.textContent = stringResult;
+
 })
